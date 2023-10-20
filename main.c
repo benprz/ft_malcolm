aef29c1fb10759b3c957bcff259ec7602ce42721
Benjamin <bperez@student.42lyon.fr>
2023-10-20 20:23:31 +0000

    switched to ubuntu, socket is well configured to receive arp packets only, and is also bound to the correct interface (which is a bridge from docker so i'm not so sure how it works out)

--- main.c
+++ main.c
@@ -6,13 +6,17 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <errno.h>
-#include <net/if.h>
+#include <linux/socket.h>
+#include <linux/if_packet.h>
 #include <net/ethernet.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
+#include <netinet/ether.h>
+#include <netinet/if_ether.h>
+#include <net/if.h>
 #include <unistd.h>
 
-#define TARGET_ADDRESS "192.168.1.2"
+#define TARGET_ADDRESS "172.17.0.3"
 
 struct ifaddrs* find_interface(struct ifaddrs *ifaddr) {
 	struct ifaddrs *ifa;
@@ -68,56 +72,84 @@ int main() {
 	}
 
 	//create socket
-	int sfd = socket(AF_, SOCK_RAW, htons(ETHERTYPE_ARP));
+	int sfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
 	if (sfd == -1) {
 		strerror(errno);
 		return 1;
 	}
 
-	//set interface index
-	unsigned int ifindex = if_nametoindex(interface->ifa_name);
-	socklen_t ifindex_len = sizeof(ifindex);
-	printf("Interface index: %d\n", ifindex);
+	printf("Socket created\n");
 
 	//bind socket to interface
-	if (setsockopt(sfd, SOL_SOCKET, IP_BOUND_IF, &ifindex, ifindex_len) == -1) {
-		strerror(errno);
+	if (setsockopt(sfd, SOL_SOCKET, SO_BINDTODEVICE, interface->ifa_name, strlen(interface->ifa_name)) == -1) {
+		strerror(errno);        
 		return 1;
 	}
 
-	//verify interface binding
-	if (getsockopt(sfd, SOL_SOCKET, IP_BOUND_IF, &ifindex, &ifindex_len) == -1) {
-		strerror(errno);
-		return 1;
+	//recvfrom broadcast address of the interface arp packets
+	struct sockaddr_ll* recv_addr = (struct sockaddr_ll*)interface->ifa_broadaddr;
+	socklen_t recv_addr_len = sizeof(recv_addr);
+	char recv_buf[ETH_FRAME_LEN];
+	int recv_len;
+
+	while (1) {
+		recv_len = recvfrom(sfd, recv_buf, ETH_FRAME_LEN, 0, (struct sockaddr *)recv_addr, &recv_addr_len);
+		if (recv_len == -1) {
+			strerror(errno);
+			return 1;
+		}
+
+		printf("Received packet: %s\n", recv_buf);
+
+		//check if the packet is an arp packet
+		struct ether_header *eth_hdr = (struct ether_header *)recv_buf;
+		if (ntohs(eth_hdr->ether_type) == ETHERTYPE_ARP) {
+			printf("Received ARP packet\n");
+
+			//check if the packet is an arp reply
+			struct ether_arp *arp_hdr = (struct ether_arp *)(recv_buf + sizeof(struct ether_header));
+			// if (ntohs(arp_hdr->ea_hdr.ar_op) == ARPOP_REPLY) {
+			// 	printf("Received ARP reply\n");
+
+			// 	//check if the packet is for the target
+			// 	struct in_addr target_addr;
+			// 	if (inet_pton(AF_INET, TARGET_ADDRESS, &target_addr) == 0) {
+			// 		strerror(errno);
+			// 		return 1;
+			// 	}
+
+			// 	if (memcmp(arp_hdr->arp_tpa, &target_addr, sizeof(target_addr)) == 0) {
+			// 		printf("Received ARP reply for target\n");
+
+			// 		//send arp reply to the sender
+			// 		struct ether_arp *arp_hdr = (struct ether_arp *)(recv_buf + sizeof(struct ether_header));
+			// 		struct ether_header *eth_hdr = (struct ether_header *)recv_buf;
+
+			// 		//swap sender and target
+			// 		memcpy(eth_hdr->ether_dhost, eth_hdr->ether_shost, ETH_ALEN);
+			// 		memcpy(eth_hdr->ether_shost, interface->ifa_addr->sa_data, ETH_ALEN);
+			// 		memcpy(arp_hdr->arp_tha, arp_hdr->arp_sha, ETH_ALEN);
+			// 		memcpy(arp_hdr->arp_sha, interface->ifa_addr->sa_data, ETH_ALEN);
+
+			// 		//set target ip to sender ip
+			// 		memcpy(arp_hdr->arp_tpa, arp_hdr->arp_spa, sizeof(arp_hdr->arp_tpa));
+
+			// 		//set sender ip to target ip
+			// 		memcpy(arp_hdr->arp_spa, &target_addr, sizeof(arp_hdr->arp_spa));
+
+			// 		//set opcode to arp reply
+			// 		arp_hdr->ea_hdr.ar_op = htons(ARPOP_REPLY);
+
+			// 		//send packet
+			// 		if (sendto(sfd, recv_buf, recv_len, 0, (struct sockaddr *)&recv_addr, sizeof(recv_addr)) == -1) {
+			// 			strerror(errno);
+			// 			return 1;
+			// 		}
+			// 		printf("Sent ARP reply\n");
+			// 	}
+			// }
+		}
 	}
-	//print interface name
-	char ifname[IF_NAMESIZE];
-	if (if_indextoname(ifindex, ifname) == NULL) {
-		strerror(errno);
-		return 1;
-	}
-	printf("Interface name: %s\n", ifname);
-
-	// struct sockaddr_in localAddress;
-	// memset(&localAddress, 0, sizeof(localAddress));
-	// localAddress.sin_family = AF_INET;
-	// localAddress.sin_addr.s_addr = INADDR_ANY; // Receive on any local interface
-	// localAddress.sin_port = htons(12345);
-
-	// if (bind(sfd, (struct sockaddr*)&localAddress, sizeof(localAddress)) == -1) {
-	// 	strerror(errno);
-	// 	return 1;
-	// }
-
-	// //recvfrom broadcast address
-	// char buffer[1024];
-	// struct sockaddr_in *fromAddress = (struct sockaddr_in *)interface->ifa_dstaddr;
-	// socklen_t fromAddressSize = sizeof(*fromAddress);
-	// if (recvfrom(sfd, buffer, sizeof(buffer), 0, (struct sockaddr*)fromAddress, &fromAddressSize) == -1) {
-	// 	strerror(errno);
-	// 	return 1;
-	// }
-
 
 	close(sfd);
 	freeifaddrs(ifaddr);
