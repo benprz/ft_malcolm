b4ebef4733760d4420724b8fec704625d294b975
Neben <neben@macben.local>
2023-10-30 04:33:12 +0100

    bonus -v + libft ; also no need to do signal, as the subject only asks to be able to use ctrl + c to quit the program

--- main.c
+++ main.c
@@ -16,10 +16,44 @@
 #include <netinet/if_ether.h>
 #include <net/if.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+#include "libft.h"
+
+bool verbose = false;
 
 void print_ethernet_header(struct ether_header *eth_hdr) {
+	if (verbose) {
 	printf("\tSource MAC: %s\n", ether_ntoa((struct ether_addr *)eth_hdr->ether_shost));
 	printf("\tTarget MAC: %s\n", ether_ntoa((struct ether_addr *)eth_hdr->ether_dhost));
+	}
+}
+
+void print_arp_header(struct ether_arp *arp_hdr, int recv_len) {
+	if (verbose) {
+	char sender_ip_str[INET_ADDRSTRLEN];
+	char target_ip_str[INET_ADDRSTRLEN];
+
+	if (inet_ntop(AF_INET, arp_hdr->arp_spa, sender_ip_str, sizeof(sender_ip_str)) == NULL) {
+		strerror(errno);
+		return ;
+	}
+	if (inet_ntop(AF_INET, arp_hdr->arp_tpa, target_ip_str, sizeof(target_ip_str)) == NULL) {
+		strerror(errno);
+		return ;
+	}
+
+	printf("\t\tHardware type: %s\n", ntohs(arp_hdr->arp_hrd) == ARPHRD_ETHER ? "Ethernet" : "Unknown");
+	printf("\t\tProtocol type: %s\n", ntohs(arp_hdr->arp_pro) == ETHERTYPE_IP ? "IPv4" : "Unknown");
+	printf("\t\tHardware size: %d\n", arp_hdr->arp_hln);
+	printf("\t\tProtocol size: %d\n", arp_hdr->arp_pln);
+	printf("\t\tOpcode: %s\n", ntohs(arp_hdr->arp_op) == ARPOP_REQUEST ? "Request" : ntohs(arp_hdr->arp_op) == ARPOP_REPLY ? "Reply" : "Unknown");
+	printf("\t\tSender MAC: %s\n", ether_ntoa((struct ether_addr *)arp_hdr->arp_sha));
+	printf("\t\tSender IP: %s\n", sender_ip_str);
+	printf("\t\tTarget MAC: %s\n", ether_ntoa((struct ether_addr *)arp_hdr->arp_tha));
+	printf("\t\tTarget IP: %s\n", target_ip_str);
+	printf("\t\tPacket length: %d\n", recv_len);
+	}
 }
 
 struct ifaddrs* find_interface(struct ifaddrs *ifaddr, struct in_addr *requested_ip_addr, struct in_addr *requester_ip_addr) {
@@ -57,27 +91,14 @@ struct ifaddrs* find_interface(struct ifaddrs *ifaddr, struct in_addr *requested
 	return NULL;
 }
 
-void print_arp_header(struct ether_arp *arp_hdr, int recv_len) {
-	printf("\tHardware type: %s\n", ntohs(arp_hdr->arp_hrd) == ARPHRD_ETHER ? "Ethernet" : "Unknown");
-	printf("\tProtocol type: %s\n", ntohs(arp_hdr->arp_pro) == ETHERTYPE_IP ? "IPv4" : "Unknown");
-	printf("\tHardware size: %d\n", arp_hdr->arp_hln);
-	printf("\tProtocol size: %d\n", arp_hdr->arp_pln);
-	printf("\tOpcode: %s\n", ntohs(arp_hdr->arp_op) == ARPOP_REQUEST ? "Request" : ntohs(arp_hdr->arp_op) == ARPOP_REPLY ? "Reply" : "Unknown");
-	printf("\tSender MAC: %s\n", ether_ntoa((struct ether_addr *)arp_hdr->arp_sha));
-	printf("\tSender IP: %s\n", inet_ntoa(*(struct in_addr *)arp_hdr->arp_spa));
-	printf("\tTarget MAC: %s\n", ether_ntoa((struct ether_addr *)arp_hdr->arp_tha));
-	printf("\tTarget IP: %s\n", inet_ntoa(*(struct in_addr *)arp_hdr->arp_tpa));
-	printf("\tPacket length: %d\n", recv_len);
-}
-
 void send_arp_reply(int sfd, struct ether_addr *requested_mac_addr, struct ether_addr *requester_mac_addr, struct in_addr *requested_ip_addr, struct in_addr *requester_ip_addr, struct sockaddr_ll *recv_addr) {
 	struct ether_header eth_hdr;
 	struct ether_arp arp_hdr;
 
 	//fill ethernet header
 	eth_hdr.ether_type = htons(ETHERTYPE_ARP);
-	memcpy(eth_hdr.ether_shost, requested_mac_addr, ETHER_ADDR_LEN);
-	memcpy(eth_hdr.ether_dhost, requester_mac_addr, ETHER_ADDR_LEN);
+	ft_memcpy(eth_hdr.ether_shost, requested_mac_addr, ETHER_ADDR_LEN);
+	ft_memcpy(eth_hdr.ether_dhost, requester_mac_addr, ETHER_ADDR_LEN);
 
 	//fill arp header
 	arp_hdr.arp_hrd = htons(ARPHRD_ETHER);
@@ -85,27 +106,27 @@ void send_arp_reply(int sfd, struct ether_addr *requested_mac_addr, struct ether
 	arp_hdr.arp_hln = ETHER_ADDR_LEN;
 	arp_hdr.arp_pln = sizeof(in_addr_t);
 	arp_hdr.arp_op = htons(ARPOP_REPLY);
-	memcpy(arp_hdr.arp_sha, requested_mac_addr, ETHER_ADDR_LEN);
-	memcpy(arp_hdr.arp_spa, requested_ip_addr, sizeof(in_addr_t));
-	memcpy(arp_hdr.arp_tha, requester_mac_addr, ETHER_ADDR_LEN);
-	memcpy(arp_hdr.arp_tpa, requester_ip_addr, sizeof(in_addr_t));
+	ft_memcpy(arp_hdr.arp_sha, requested_mac_addr, ETHER_ADDR_LEN);
+	ft_memcpy(arp_hdr.arp_spa, requested_ip_addr, sizeof(in_addr_t));
+	ft_memcpy(arp_hdr.arp_tha, requester_mac_addr, ETHER_ADDR_LEN);
+	ft_memcpy(arp_hdr.arp_tpa, requester_ip_addr, sizeof(in_addr_t));
 
 	//fill buffer
 	char buf[ETHER_HDR_LEN + sizeof(struct ether_arp)];
-	memcpy(buf, &eth_hdr, sizeof(struct ether_header));
-	memcpy(buf + sizeof(struct ether_header), &arp_hdr, sizeof(struct ether_arp));
+	ft_memcpy(buf, &eth_hdr, sizeof(struct ether_header));
+	ft_memcpy(buf + sizeof(struct ether_header), &arp_hdr, sizeof(struct ether_arp));
 
 	//print packet
-	printf("Sending packet:\n");
+	printf("\tSending reply\n");
 	print_ethernet_header(&eth_hdr);
 	print_arp_header(&arp_hdr, sizeof(buf));
 
 	//send packet to requester
 	if (sendto(sfd, buf, sizeof(buf), 0, (struct sockaddr *)recv_addr, sizeof(struct sockaddr_ll)) == -1) {
 		strerror(errno);
-		exit(1);
+		return ;
 	}
-	printf("Sent packet!\n");
+	printf("\t\tSent reply!\n");
 }
 
 void handle_arp_packets(int sfd, struct ether_addr *requested_mac_addr, struct ether_addr *requester_mac_addr, struct in_addr *requested_ip_addr, struct in_addr *requester_ip_addr) {
@@ -120,46 +141,28 @@ void handle_arp_packets(int sfd, struct ether_addr *requested_mac_addr, struct e
 		recv_len = recvfrom(sfd, recv_buf, ETH_FRAME_LEN, 0, (struct sockaddr *)&recv_addr, &recv_addr_len);
 		if (recv_len == -1) {
 			strerror(errno);
-			exit(1);
+			return ;
 		}
 		printf("Received packet!\n");
-		//check if the packet is an arp packet
 		struct ether_header *eth_hdr = (struct ether_header *)recv_buf;
 		struct ether_arp *arp_hdr = (struct ether_arp *)(recv_buf + sizeof(struct ether_header));
 
 		//print mac addresses from ethernet header
 		print_ethernet_header(eth_hdr);
 
-		if (ntohs(eth_hdr->ether_type) == ETHERTYPE_ARP) {
+		// check if the packet is an arp request
+		if (ntohs(arp_hdr->arp_op) == ARPOP_REQUEST) {
+
+			printf("\tIt's a request!\n");
 			print_arp_header(arp_hdr, recv_len);
+
 			//check if the packet is for us
-			char sender_ip[INET_ADDRSTRLEN];
-			char target_ip[INET_ADDRSTRLEN];
-			if (inet_ntop(AF_INET, arp_hdr->arp_spa, sender_ip, sizeof(sender_ip)) == NULL) {
-				strerror(errno);
-				exit(1);
-			}
-			if (inet_ntop(AF_INET, arp_hdr->arp_tpa, target_ip, sizeof(target_ip)) == NULL) {
-				strerror(errno);
-				exit(1);
-			}
-			if (strcmp(sender_ip, inet_ntoa(*requester_ip_addr)) == 0 && strcmp(target_ip, inet_ntoa(*requested_ip_addr)) == 0) {
+			if (ft_memcmp(&arp_hdr->arp_spa, requester_ip_addr, sizeof(struct in_addr)) == 0 &&
+				ft_memcmp(&arp_hdr->arp_tpa, requested_ip_addr, sizeof(struct in_addr)) == 0) {
 				send_arp_reply(sfd, requested_mac_addr, requester_mac_addr, requested_ip_addr, requester_ip_addr, &recv_addr);
 				break;
 			}
 		}
-	}
-}
-
-void bind_socket_to_interface(int sfd, struct ifaddrs *ifaddr, struct in_addr *requested_ip_addr, struct in_addr *requester_ip_addr) {
-	struct ifaddrs *interface;
-	if ((interface = find_interface(ifaddr, requested_ip_addr, requester_ip_addr)) == NULL) {
-		exit(1);
-	}
-
-	if (setsockopt(sfd, SOL_SOCKET, SO_BINDTODEVICE, interface->ifa_name, strlen(interface->ifa_name)) == -1) {
-		strerror(errno);
-		exit(1);
 	}
 }
 
@@ -168,56 +171,59 @@ void create_socket(struct ether_addr *requested_mac_addr, struct ether_addr *req
 	int sfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
 	if (sfd == -1) {
 		strerror(errno);
-		exit(1);
+		return;
 	}
 
-	printf("Socket created\n");
-
-	//bind socket to interface
-	struct ifaddrs *ifaddr;
+	//find interface
+	struct ifaddrs *ifaddr, *interface;
 	if (getifaddrs(&ifaddr) == -1) {
 		strerror(errno);
-		exit(1);
+		return;
 	}
-	bind_socket_to_interface(sfd, ifaddr, requested_ip_addr, requester_ip_addr);
+	if ((interface = find_interface(ifaddr, requested_ip_addr, requester_ip_addr)) == NULL) {
+		return ;
+	}
+
+	//set interface to socket
+	if (setsockopt(sfd, SOL_SOCKET, SO_BINDTODEVICE, interface->ifa_name, strlen(interface->ifa_name)) == -1) {
+		strerror(errno);
+		return ;
+	}
+	freeifaddrs(ifaddr);
 
 	//handle arp packets
 	handle_arp_packets(sfd, requested_mac_addr, requester_mac_addr, requested_ip_addr, requester_ip_addr);
 
+	//close socket
 	close(sfd);
-	freeifaddrs(ifaddr);
 }
 
 //function to convert a string to a mac address as ether_addr
 int ft_ether_aton(const char *asc, struct ether_addr *haddr) {
-    uint8_t addr[6];
-
     // Check the length of the input string
     if (strlen(asc) != 17) {
         return 0; // Invalid MAC address length
     }
 
-    // Use sscanf to parse the input string
-    if (sscanf(asc, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
-        &addr[0], &addr[1], &addr[2],
-        &addr[3], &addr[4], &addr[5]) != 6) {
-        return 0; // Invalid MAC address format
-    }
+	char nb[3];
+	nb[2] = '\0';
 
     // Check if each byte of the MAC address is within a valid range
     for (int i = 0; i < 6; ++i) {
-        if (addr[i] > 255) {
-            return 0; // Invalid byte value
-        }
-        haddr->ether_addr_octet[i] = addr[i];
+		nb[0] = asc[i * 3];
+		nb[1] = asc[i * 3 + 1];
+		if (ft_isalnum(nb[0]) == 0 || ft_isalnum(nb[1]) == 0) {
+			return 0; // Invalid MAC address
+		}
+		haddr->ether_addr_octet[i] = ft_strtol(nb, NULL, 16);
     }
 
     return 1; // Valid MAC address
 }
 
 int main(int ac, char **av) {
-	if (ac != 5) {
-		printf("Usage: %s <requested_ip_address> <requested_mac_address> <requester_ip_address> <requester_mac_address>\n", av[0]);
+	if (ac < 5) {
+		printf("Usage: %s <requested_ip_address> <requested_mac_address> <requester_ip_address> <requester_mac_address> [-v]\n", av[0]);
 		return 1;
 	}
 
@@ -243,6 +249,9 @@ int main(int ac, char **av) {
 		return 1;
 	}
 
+	if (ac == 6 && strcmp(av[5], "-v") == 0) {
+		verbose = true;
+	}
 
 	create_socket(&requested_mac_addr, &requester_mac_addr, &requested_ip_addr, &requester_ip_addr);
 
